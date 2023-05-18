f5629b967fe79b695d5ccc96096ca4f4c18e5cf7
peppone-choi <peppone.choi@gmail.com>
2023-05-18 16:44:32 +0900

    회사 조회까지 구현.

---
+++ src/main/java/com/zerobase/dividend/web/CompanyController.java
@@ -0,0 +1,54 @@
+package com.zerobase.dividend.web;
+
+import com.zerobase.dividend.model.Company;
+import com.zerobase.dividend.persist.entity.CompanyEntity;
+import com.zerobase.dividend.service.CompanyService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/company")
+@AllArgsConstructor
+public class CompanyController {
+
+    private final CompanyService companyService;
+
+    @GetMapping("/autocomplete")
+    public ResponseEntity<?> autocomplete(@RequestParam String keyword) {
+        return null;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> searchCompany(final Pageable pageable){
+        Page<CompanyEntity> companies = this.companyService.getAllCompany(pageable);
+        return ResponseEntity.ok(companies);
+    }
+
+    /**
+     * 회사 및 배당금 정보 추가
+     * @param request
+     * @return
+     */
+    @PostMapping
+    public ResponseEntity<?> addCompany(@RequestBody Company request){
+        String ticker = request.getTicker().trim();
+        if (ObjectUtils.isEmpty(ticker)) {
+            throw new RuntimeException("ticker is empty");
+        }
+
+        Company company = this.companyService.save(ticker);
+
+        return ResponseEntity.ok(company);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteCompany(){
+        return null;
+    }
+}
