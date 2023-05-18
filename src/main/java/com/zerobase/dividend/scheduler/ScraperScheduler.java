465eb98382b6c8cb38c73aa11475372223ab5917
peppone-choi <peppone.choi@gmail.com>
2023-05-18 22:39:23 +0900

    스케쥴러로 특정 시간 스크래핑 까지 구현.

---
+++ src/main/java/com/zerobase/dividend/scheduler/ScraperScheduler.java
@@ -0,0 +1,60 @@
+package com.zerobase.dividend.scheduler;
+
+import com.zerobase.dividend.model.Company;
+import com.zerobase.dividend.model.ScrapedResult;
+import com.zerobase.dividend.persist.CompanyRepository;
+import com.zerobase.dividend.persist.DividendRepository;
+import com.zerobase.dividend.persist.entity.CompanyEntity;
+import com.zerobase.dividend.persist.entity.DividendEntity;
+import com.zerobase.dividend.scraper.Scraper;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class ScraperScheduler {
+    private final CompanyRepository companyRepository;
+    private final DividendRepository dividendRepository;
+
+    private final Scraper yahooFinanceScraper;
+
+
+
+    // 일정 주기마다 수행
+    @Scheduled(cron = "${scheduler.scrap.yahoo}")
+    public void yahooFinanceScheduling() {
+        log.info("scraping scheduler is started");
+        // 저장한 회사 목록 조회
+        List<CompanyEntity> companies = this.companyRepository.findAll();
+        // 회사마다 정보를 새로 스크래핑
+        for (var company : companies) {
+            log.info("scraping scheduler is started ->" + company.getName());
+            ScrapedResult scrapedResult = this.yahooFinanceScraper.scrap(Company.builder()
+                    .name(company.getName())
+                    .ticker(company.getTicker())
+                    .build());
+            // 스크래핑한 배당금 정보 중 데이터베이스 값에 없는 값은 저장
+            scrapedResult.getDividends().stream()
+                    // 디비든 모델을 디비든 모델로 매핑
+                    .map(e -> new DividendEntity(company.getId(), e))
+                    // 엘리먼트를 하나씩 디비든 레파지토리에 삽입.
+                    .forEach(e -> {
+                        boolean exists = this.dividendRepository.existsByCompanyIdAndDate(e.getCompanyId(), e.getDate());
+                        if (!exists) {
+                            this.dividendRepository.save(e);
+                        }
+                    });
+            // 연속적인 스크래핑 요청 방지를 위한 일시 정지
+            try {
+                Thread.sleep(3000); // 3 seconds.
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
